d9666f0b01e566657b4354901ae504438d9705a2
Jaroslav Páral <paral.jarek@gmail.com>
2017-12-12 22:10:40 +0100

    2017-12-05 + 2017-12-12: update description of the source code

--- 2017-12-05_serial_control/2017-12-05_serial_control.ino
+++ 2017-12-05_serial_control/2017-12-05_serial_control.ino
@@ -1,34 +1,38 @@
-#include <LearningKit.h>
+#include <LearningKit.h> // naše knihovna pro vývojový kit ALKS - verze pro ESP32: http://platformio.org/lib/show/1745/ArduinoLearkingKitStarter
+// návod na instalace do Arduino IDE: https://github.com/RoboticsBrno/RoboticsBeginners2017-2018/
 
-//static const int pin = L_R;
-int pin = L_R;
+// když chci pracovat s piny na Arduinu, je lepší si definovat proměnnou s označující daný pin a přiřadit do ní číslo pinu
+int pin = L_R; // jednoduchá, ale schůdná varianta
+//static const int pin = L_R; // lepší forma definice proměnné pro pin -> "rychlejší" vykonávání programu
 
 void setup() {
-  //pinMode(pin, OUTPUT); // lze nastavit jednotlivé piny
-  setupLeds();            // nebo můžete využít funkci v naší knihovně
+  // v setup() je potřeba inicializovat všechny piny Arduina, s kterými budeme pracovat
+  //pinMode(pin, OUTPUT); // lze je nastavit po jednotlivých pinech pomocí funkce pinMode(cislo_pinu, OUTPUT/INPUT/INPUT_PULLUP);
+  setupLeds();            // nebo lze využít funkce v knihovně pro náš vývojový kit ALKS (setupLeds(), setupRgbLed(), setupButtons())
 
   // Seril.begin() -> musí být vždy v setup() pokud chceme používat Serial Monitor (komunikace mezi PC a Arduinem)
-  Serial.begin(9600); // otevře seriový port, komunikační linku mezi Arduinem a PC, komunikační rychlost 9600 bps (bity za sekundu -> běžně přednastavená rychlost u Seriového monitoru v Arduino prostředích)
+  Serial.begin(9600); // otevře sériový port, komunikační linku mezi Arduinem a PC, komunikační rychlost 9600 bps (bity za sekundu 
+                      // -> běžně přednastavená rychlost u sériového monitoru v Arduino prostředích: Tools -> Serial Monitor)
   Serial.print(65);   // odešle dané číslo do PC -> zobrazí "65"
   Serial.println(65); // odešle dané číslo do PC a přidá nový řádek
   Serial.write(65);   // zobrazí znak odpovídající dané ASCII hodnotě -> 65 = 'A' (více o ASCII: https://cs.wikipedia.org/wiki/ASCII)
-  Serial.println(" Ahoj lidi"); 
+  Serial.println(" Ahoj lidi"); // odešle z Arduina textový řetězec " Ahoj lidi"
 }
 
-int znak; // proměnná do které se ukládají přichozí znaky
+int znak; // proměnná do které se ukládají příchozí znaky
 void loop() {
   if (Serial.available()) // Serial.available() - řekne, zda jsou dostupná nějaká data na sériové lince: true 
   {
-    znak = Serial.read(); // když jsou data dostupné (Serial.available()), přečtu jeden znak (Serial.read())
-    Serial.write(znak);
-    Serial.write('\n');
+    znak = Serial.read(); // když jsou data dostupné -> Serial.available() == true -> přečtu jeden znak -> Serial.read()
+    Serial.write(znak);   // pro kontrolu odešlu znak zpět po sériové lince do PC
+    Serial.write('\n');   // + odešlu nový řádek -> '\n'
       
     // 0 -> vypnu LED
     // 1 -> zapnu LED
-    if(znak == '0') {
+    if(znak == '0') { // pokud přišel znak '0' -> vypnu LED
       Serial.println(": Vypnu LED 1");
       digitalWrite(pin, LOW);
-    } else if(znak == '1') {
+    } else if(znak == '1') { // pokud přišel znak '1' -> ...
       Serial.println(": Zapnu LED 1");
       digitalWrite(pin, HIGH);
     } else if(znak == 'z') {
--- 2017-12-12_serial_control/2017-12-12_serial_control.ino
+++ 2017-12-12_serial_control/2017-12-12_serial_control.ino
@@ -1,23 +1,27 @@
-#include <LearningKit.h>
+#include <LearningKit.h> // naše knihovna pro vývojový kit ALKS - verze pro ESP32: http://platformio.org/lib/show/1745/ArduinoLearkingKitStarter
+// návod na instalace do Arduino IDE: https://github.com/RoboticsBrno/RoboticsBeginners2017-2018/
 
-//static const int pin = L_R;
-int pin = L_R;
+// když chci pracovat s piny na Arduinu, je lepší si definovat proměnnou s označující daný pin a přiřadit do ní číslo pinu
+int pin = L_R; // jednoduchá, ale schůdná varianta
+//static const int pin = L_R; // lepší forma definice proměnné pro pin -> "rychlejší" vykonávání programu
 
 void setup() {
-  //pinMode(pin, OUTPUT); // lze nastavit jednotlivé piny
-  setupLeds();            // nebo můžete využít funkci v naší knihovně
+  // v setup() je potřeba inicializovat všechny piny Arduina, s kterými budeme pracovat
+  //pinMode(pin, OUTPUT); // lze je nastavit po jednotlivých pinech pomocí funkce pinMode(cislo_pinu, OUTPUT/INPUT/INPUT_PULLUP);
+  setupLeds();            // nebo lze využít funkce v knihovně pro náš vývojový kit ALKS (setupLeds(), setupRgbLed(), setupButtons())
 
   // Seril.begin() -> musí být vždy v setup() pokud chceme používat Serial Monitor (komunikace mezi PC a Arduinem)
-  Serial.begin(9600); // otevře seriový port, komunikační linku mezi Arduinem a PC, komunikační rychlost 9600 bps (bity za sekundu -> běžně přednastavená rychlost u Seriového monitoru v Arduino prostředích)
-  Serial.println(" Ahoj lidi"); 
+  Serial.begin(9600); // otevře sériový port, komunikační linku mezi Arduinem a PC, komunikační rychlost 9600 bps (bity za sekundu 
+                      // -> běžně přednastavená rychlost u sériového monitoru v Arduino prostředích: Tools -> Serial Monitor)
+  Serial.println(" Ahoj lidi"); // odešle z Arduina textový řetězec " Ahoj lidi"
 }
 
-int znak; // proměnná do které se ukládají přichozí znaky
-bool ledStav = false;
+int znak; // proměnná do které se ukládají příchozí znaky
+bool ledStav = false; // proměnná pro ukládání stavu LED -> využíváme k zapínání a vypínání LED jedním znakem (rozdíl oproti předchozí lekci 2017-12-05)
 void loop() {
-  if(Serial.available()) {
-    znak = Serial.read();
-    if(znak == 'c') {                   // pokud příjde znak 'c'
+  if(Serial.available()) {            // pokud přišla nějaká data na sériovou linku je podmínka splněna (vrátí TRUE)
+    znak = Serial.read();               // přečte jeden znak ze sériové linky a uloží jej do proměnné znak
+    if(znak == 'c') {                   // pokud přijde znak 'c'
       if(ledStav == false) {              // a ledStav je roven FALSE
         ledStav = true;                     // ledStav se nastaví na TRUE -> digitalWrite() na konci programu zapne LED
         Serial.println("LED zapnuta");      // a na sériové lince se vypíše text "LED zapnuta"    
@@ -25,15 +29,16 @@ void loop() {
         ledStav = false;                    // ledStav se nastaví na FALSE -> digitalWrite() na konci programu vypne LED
         Serial.println("LED vypnuta");      // a na sériové lince se vypíše text "LED zapnuta"
       }
-    } else {                            // pokud nepříjde znak 'c' -> vypíše se info na sériovou linku
+    } else {                            // pokud nepřijde znak 'c' -> vypíše se info na sériovou linku
       Serial.print("Poslal jste spatny znak: ");
       Serial.write(znak);
       Serial.print("\nPro ovladani LED poslete 'c'\n");  
     }
   }
-  digitalWrite(L_G, ledStav); // nastaví (zapne/vypne) LED podle promněnné ledStav
+  // při každém průchodu smyčkou se nastaví pin L_G
+  digitalWrite(L_G, ledStav); // nastaví (zapne/vypne) LED podle proměnné ledStav
 
-//  // ukázka jednoduchého zapínání a vypínání (== blikání) LED
+//  // ukázka jednoduchého zapínání a vypínání LED pomocí tlačítka -> pro připomenutí a porovnání s řešením výše
 //  if(digitalRead(SW1) == LOW) {
 //    digitalWrite(L_G, HIGH);
 //    Serial.println("LED zapnuta");    

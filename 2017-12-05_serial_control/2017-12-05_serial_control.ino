8ca041ff4c698e6771214295eeb30bcbee6668a9
Jarek Páral <paral.jarek@gmail.com>
2017-12-12 18:01:00 +0100

    2017-12-05_serial_control: update description

--- 2017-12-05_serial_control/2017-12-05_serial_control.ino
+++ 2017-12-05_serial_control/2017-12-05_serial_control.ino
@@ -6,20 +6,23 @@ int pin = L_R;
 void setup() {
   //pinMode(pin, OUTPUT); // lze nastavit jednotlivé piny
   setupLeds();            // nebo můžete využít funkci v naší knihovně
-  
-  Serial.begin(9600);
-  Serial.print(65);
-  Serial.write(65);
-  Serial.println(" Ahoj lidi");
+
+  // Seril.begin() -> musí být vždy v setup() pokud chceme používat Serial Monitor (komunikace mezi PC a Arduinem)
+  Serial.begin(9600); // otevře seriový port, komunikační linku mezi Arduinem a PC, komunikační rychlost 9600 bps (bity za sekundu -> běžně přednastavená rychlost u Seriového monitoru v Arduino prostředích)
+  Serial.print(65);   // odešle dané číslo do PC -> zobrazí "65"
+  Serial.println(65); // odešle dané číslo do PC a přidá nový řádek
+  Serial.write(65);   // zobrazí znak odpovídající dané ASCII hodnotě -> 65 = 'A' (více o ASCII: https://cs.wikipedia.org/wiki/ASCII)
+  Serial.println(" Ahoj lidi"); 
 }
 
-int znak;
+int znak; // proměnná do které se ukládají přichozí znaky
 void loop() {
-  if (Serial.available()) {
-    znak = Serial.read();
+  if (Serial.available()) // Serial.available() - řekne, zda jsou dostupná nějaká data na sériové lince: true 
+  {
+    znak = Serial.read(); // když jsou data dostupné (Serial.available()), přečtu jeden znak (Serial.read())
     Serial.write(znak);
     Serial.write('\n');
-    
+      
     // 0 -> vypnu LED
     // 1 -> zapnu LED
     if(znak == '0') {

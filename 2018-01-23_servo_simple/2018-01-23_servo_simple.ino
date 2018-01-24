664c6df71e0f4db3f27be6dca72745c3c9659977
Jarek Páral <paral.jarek@gmail.com>
2018-01-24 11:23:54 +0100

    2018-01-23_servo_simple: add some description

--- 2018-01-23_servo_simple/2018-01-23_servo_simple.ino
+++ 2018-01-23_servo_simple/2018-01-23_servo_simple.ino
@@ -1,5 +1,9 @@
 #include <LearningKit.h>
 
+// Generování PWM signálu pomocí delay()/ delayMicroseconds()
+// https://en.wikipedia.org/wiki/Servo_control
+// https://en.wikipedia.org/wiki/Servo_control#/media/File:ServoPwm.png
+
 void setup() {
   pinMode(S1, OUTPUT);
 }
@@ -8,7 +12,7 @@ void loop() {
   int sig = analogRead(POT1); // 0 - 4096
   digitalWrite(S1, HIGH);
   delayMicroseconds(1000);
-  delayMicroseconds(sig/4);
+  delayMicroseconds(sig/4);   // sig/4 = 0 - 1024 
   digitalWrite(S1, LOW);
   delayMicroseconds(19000 - (sig/4));
 }
